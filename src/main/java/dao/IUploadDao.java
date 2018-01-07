cbdd31090d46d0adbf60e0d5d84a199073a05847
Samarithan <alexandru.vasiliu2017@gmail.com>
2018-01-07 21:22:54 +0200

    writing file on server

---
+++ src/main/java/dao/IUploadDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import entities.File;
+import org.postgresql.util.PSQLException;
+
+import javax.ejb.Local;
+import java.util.List;
+
+@Local
+public interface IUploadDao {
+
+    void addFile(File uploadFile) throws PSQLException;
+
+    public List<File> getUploadedFiles();
+
+}
---
+++ src/main/java/dao/UploadDao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import entities.File;
+import org.postgresql.util.PSQLException;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Stateless
+public class UploadDao implements IUploadDao {
+    @PersistenceContext(unitName = "postgres") private EntityManager entityManager;
+
+    @Override
+    public void addFile(File uploadFile) throws PSQLException {
+        entityManager.persist(uploadFile);
+    }
+
+    @Override
+    public List<File> getUploadedFiles() {
+        Query query = entityManager.createQuery("SELECT f FROM File f ");
+        return query.getResultList(); //???????????
+    }
+}
